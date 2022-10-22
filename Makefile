67e3cd8b4c8318e7a0488554604603a926cf9933
Rukkyaa <lbrt.axel@gmail.com>
2022-10-22 13:49:35 +0200

    Bonus added and my tester is working

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: rukkyaa <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/18 17:19:09 by rukkyaa           #+#    #+#              #
-#    Updated: 2022/10/21 22:28:09 by rukkyaa          ###   ########.fr        #
+#    Updated: 2022/10/22 13:04:16 by rukkyaa          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,9 +18,14 @@ SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 		ft_memcmp.c ft_atoi.c ft_calloc.c ft_strdup.c ft_putnbr_fd.c \
 		ft_strnstr.c ft_memmove.c ft_substr.c ft_strjoin.c ft_strmapi.c \
 		ft_strtrim.c ft_split.c ft_itoa.c ft_striteri.c ft_putchar_fd.c \
-		ft_putstr_fd.c ft_putendl_fd.c ft_strlcat.c ft_strlcpy.c main.c
+		ft_putstr_fd.c ft_putendl_fd.c ft_strlcat.c ft_strlcpy.c #main.c
+
+BONUS_SRC = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c \
+		ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c
 
 OBJS = ${SRC:.c=.o}
+
+BONUS_OBJS = $(BONUS_SRC:.c=.o)
 
 INCLUDE = includes/
 
@@ -32,12 +37,15 @@ all: ${NAME}
 $(NAME): ${OBJS}
 	ar -rcs ${NAME} ${OBJS}
 
+bonus: $(OBJS) $(BONUS_OBJS)
+	ar -rcs $(NAME) $(OBJS) $(BONUS_OBJS)
+
 clean:
-	rm -f ${OBJS}
+	rm -f ${OBJS} $(BONUS_OBJS)
 
 fclean: clean
 	rm -f ${NAME}
 
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all bonus clean fclean re
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rukkyaa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 15:51:47 by rukkyaa           #+#    #+#             */
-/*   Updated: 2022/10/21 22:28:21 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2022/10/22 13:05:19 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
 
 int		ft_atoi(const char *nptr);
 int		ft_isdigit(int c);
@@ -26,6 +32,7 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_lstsize(t_list *lst);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
@@ -44,6 +51,11 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_bzero(void *s, size_t n);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
@@ -54,5 +66,9 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
